9dae8a1e7ba6a47e41a28b95c902261519899139
Nathan Nuckels <nathannuckels@gmail.com>
2022-03-05 16:45:25 -0600

    Didnt get much progress done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+
 public class Main {
+
     public static void Main(String[] args){
         System.out.println("Welcome to FloofChat");
-
+        System.out.println();
     }
 }
